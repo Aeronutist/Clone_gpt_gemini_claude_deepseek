acea9d7e8b381a8d409fdf2003d2a32714cab853
Aero nutist <aeronutist22@gmail.com>
2025-07-18 19:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+ import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
